22167be819e82adf0d2c16ba737dbea73af67952
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2023-02-28 19:31:12 +0100

    Create room DB (dao and entity)

---
+++ app/src/main/java/com/psa/weatherapp/weather_feature/data/Weather.kt
@@ -0,0 +1,24 @@
+package com.psa.weatherapp.weather_feature.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "weather")
+data class Weather  @JvmOverloads  constructor(
+    @PrimaryKey @ColumnInfo(name = "cityid") var id:String = "",
+    @ColumnInfo(name = "cityname") var cityName: String = "",
+    @ColumnInfo(name = "country") var country: String = "",
+    @ColumnInfo(name = "description") var description: String = "",
+    @ColumnInfo(name = "icon") var icon: String = "",
+    @ColumnInfo(name = "temp") var temperature: String = "",
+    @ColumnInfo(name = "feelslike") var feelsLike: String = "",
+    @ColumnInfo(name = "tempmin") var tempMin: String = "",
+    @ColumnInfo(name = "tempmax") var tempMax: String = "",
+    @ColumnInfo(name = "humidity") var humidity: String = "",
+    @ColumnInfo(name = "windspeed") var windSpeed: String = "",
+    @ColumnInfo(name = "winddeg") var windDegree: String = "",
+) {
+    val isEmpty
+        get() = cityName.isEmpty() || temperature.isEmpty()
+}
---
+++ app/src/main/java/com/psa/weatherapp/weather_feature/data/source/local/WeatherDao.kt
@@ -0,0 +1,37 @@
+package com.psa.weatherapp.weather_feature.data.source.local
+
+import androidx.room.*
+import com.psa.weatherapp.weather_feature.data.Weather
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WeatherDao {
+
+    /**
+     * Insert a city weather object in the database. If the city weather already exists, replace it.
+     *
+     * @param cityWeather the cityWeather to be inserted.
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCityWeather(cityWeather: Weather)
+
+    /**
+     * Observes a single weather object.
+     *
+     * @param cityId the city id.
+     * @return the weather of the city with cityId.
+     */
+    @Query("SELECT * FROM Weather WHERE cityid = :cityId")
+    fun observeCityWeatherById(cityId: String): Flow<Weather>
+
+
+    /**
+     * Delete a weather object by id.
+     *
+     * @return the number of weather object deleted. This should always be 1.
+     */
+    @Query("DELETE FROM Weather WHERE cityid = :cityId")
+    suspend fun deleteCityWeatherById(cityId: String): Int
+
+
+}
