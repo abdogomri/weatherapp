87d140490a6cf52b49e05a9815906de4ad70c850
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2023-03-01 14:13:32 +0100

    - add datasource modules to DI

--- openweathermaplibrary/src/main/java/com/psa/openweathermaplibrary/di/DataModules.kt
+++ openweathermaplibrary/src/main/java/com/psa/openweathermaplibrary/di/DataModules.kt
@@ -1,25 +1,85 @@
-package com.psa.weatherapp.di
+package com.psa.openweathermaplibrary.di
 
 import android.content.Context
 import androidx.room.Room
 import com.psa.openweathermaplibrary.data.ApiInterface
 import com.psa.openweathermaplibrary.data.ApiInterfaceContainer
+import com.psa.openweathermaplibrary.weather_feature.data.source.DefaultWeatherRepository
+import com.psa.openweathermaplibrary.weather_feature.data.source.WeatherDataSource
+import com.psa.openweathermaplibrary.weather_feature.data.source.WeatherRepository
 import com.psa.openweathermaplibrary.weather_feature.data.source.local.WeatherDatabase
+import com.psa.openweathermaplibrary.weather_feature.data.source.local.WeatherLocalDataSource
+import com.psa.openweathermaplibrary.weather_feature.data.source.remote.WeatherRemoteDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RemoteWeatherDataSource
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LocalWeatherDataSource
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DatabaseModule {
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideWeatherRepository(
+        @RemoteWeatherDataSource remoteDataSource: WeatherDataSource,
+        @LocalWeatherDataSource localDataSource: WeatherDataSource,
+    ): WeatherRepository {
+        return DefaultWeatherRepository(remoteDataSource, localDataSource)
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataSourceModule {
+    @Singleton
+    @RemoteWeatherDataSource
+    @Provides
+    fun provideWeatherRemoteDataSource(
+        api: ApiInterface
+    ): WeatherDataSource  {
+        return WeatherRemoteDataSource(api)
+    }
+
+    @Singleton
+    @LocalWeatherDataSource
+    @Provides
+    fun provideWeatherLocalDataSource(
+        database: WeatherDatabase,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): WeatherDataSource {
+        return WeatherLocalDataSource(database.weatherDao(), ioDispatcher)
+    }
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteDataModule {
 
     @Provides
     fun provideOnlineApiInterface(container: ApiInterfaceContainer): ApiInterface {
         return container.provideApiInterface()
     }
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
 
     @Singleton
     @Provides
